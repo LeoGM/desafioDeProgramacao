9ff53a8ed75353a91e04c43735d2ac9ad4c3b428
LeoGM <leograpperm@gmail.com>
2022-02-20 22:11:26 -0300

    classe principal

---
+++ desafiodeprogramacao/src/main/java/com/academiacapgemini/desafiodeprogramacao/Main.java
@@ -0,0 +1,8 @@
+package com.academiacapgemini.desafiodeprogramacao;
+
+public class Main {
+    public static void main(String[] args) {
+        Login novoLogin = new Login();
+        novoLogin.iniciarPrograma();
+    }
+}
